4b1bf547cc847b45d08be0e50deb6f2a293cc16c
FairTex <makarov906@gmail.com>
2015-11-17 17:51:13 +0500

    upd

--- src/check.js
+++ src/check.js
@@ -8,15 +8,15 @@ exports.init = function () {
             }
         }
         return true;
-    }
+    };
 
     Object.prototype.checkHasKeys = function (keys) {
         var thisArr = this;
-        var filter = keys.filter(function(key) {
+        var filter = keys.filter(function (key) {
             return (key in thisArr);
         });
         return filter.length == Object.keys(thisArr).length;
-    }
+    };
 
     Object.prototype.checkContainsValues = function (values) {
         for (var value of values) {
@@ -25,30 +25,30 @@ exports.init = function () {
             }
         }
         return true;
-    }
+    };
 
     Object.prototype.checkHasValues = function (values) {
         var arr = this;
-        var filter = values.filter(function(value) {
+        var filter = values.filter(function (value) {
             return (arr.indexOf(value) > 0);
         });
         return filter.length == Object.keys(arr).length;
-    }
+    };
 
     Object.prototype.checkHasValueType = function (key, type) {
         return typeof this[key] == type.name.toLowerCase();
-    }
+    };
 
     Object.prototype.checkHasLength = function (length) {
         return this.length == length;
-    }
+    };
 
     Function.prototype.checkHasParamsCount = function (count) {
         return this.length == count;
-    }
+    };
 
     String.prototype.checkHasWordsCount = function (count) {
         var words = this.split(' ');
         return words.length == count;
-    }
+    };
 };
